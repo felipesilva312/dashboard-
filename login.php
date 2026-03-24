648da77533128e14dbbca401c888711340e4003c
Felipe Abreu <felipesilva203040@gmail.com>
2026-03-23 22:18:01 -0300

    Painel login acrescentado

---
+++ login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dash.css">
+    <link rel="stylesheet" href=https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/brands.min.css>
+    <title>Login</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+
+
+<head></head>
+
+<body>
+
+<div class="login-container">
+
+    <div class="login-card">
+
+        <div class="login-header">
+            <i class="fa-solid fa-newspaper logo-icon"></i>
+            <h2>Painel do Blog</h2>
+        </div>
+
+        <form action="valida_login.php" method="POST">
+
+            <div class="form-group">
+                <input type="email" name="email" placeholder="E-mail" required>
+            </div>
+
+            <div class="form-group">
+                <input type="password" name="senha" placeholder="Senha" required>
+            </div>
+
+            <button type="submit" class="btn-login">Acessar Painel</button>
+
+        </form>
+
+    </div>
+
+</div>
+
+</body>
